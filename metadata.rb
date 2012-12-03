90c68b4464a0d4d3b7148fa64a1fd9e56951a6c2
Peter Donald <peter@realityforge.org>
2012-12-03 15:00:53 +1100

    Use ' rather than '

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'cutlery'
-maintainer       "Peter Donald"
-maintainer_email "peter@realityforge.org"
-license          "Apache 2.0"
-description      "Cutlery is a cookbook containing a collection useful library code."
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
+license          'Apache 2.0'
+description      'Cutlery is a cookbook containing a collection useful library code.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.1.0'
