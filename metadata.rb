41f8db8fd0b459d614c70751ee5de98bba8f3d19
Peter Donald <peter@realityforge.org>
2013-05-03 14:56:53 +1000

    Bump the version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'peter@realityforge.org'
 license          'Apache 2.0'
 description      'Cutlery is a cookbook containing a collection useful library code.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.2'
+version          '0.2.3'
